0c5c2bd3b67dd809a6c3c55c68b229cbe81f9871
fosb <fosbtank@gmail.com>
2025-03-12 20:51:17 +0000

    Inital commit

---
+++ Services/Storage.cs
@@ -0,0 +1,22 @@
+﻿using System.Xml;
+using LifeTasker.Models;
+using Newtonsoft.Json;
+
+namespace LifeTasker.Services
+{
+    public static class Storage
+    {
+        private static readonly string _filePath = "tasks.json";
+
+        public static void SaveData(List<LifeTask> tasks)
+        {
+            File.WriteAllText(_filePath, JsonConvert.SerializeObject(tasks, Newtonsoft.Json.Formatting.Indented));
+        }
+
+        public static List<LifeTask> LoadData()
+        {
+            if (!File.Exists(_filePath)) return new List<LifeTask>();
+            return JsonConvert.DeserializeObject<List<LifeTask>>(File.ReadAllText(_filePath));
+        }
+    }
+}
