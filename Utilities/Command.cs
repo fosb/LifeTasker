260b2a2f6024c45b235bf9bade62a676ebc8153e
fosb <fosbtank@gmail.com>
2025-03-23 19:24:54 +0000

    Refactor to correct issues with MVVP pattern

---
+++ Utilities/Command.cs
@@ -0,0 +1,14 @@
+﻿public class Command
+{
+    private readonly Action<object> _execute;
+
+    public Command(Action<object> execute)
+    {
+        _execute = execute;
+    }
+
+    public void Execute(object parameter)
+    {
+        _execute(parameter);
+    }
+}
