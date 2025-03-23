8636db2c522983773392b572c7ffeccd0fd9a6b9
fosb <fosbtank@gmail.com>
2025-03-23 13:21:16 +0000

    Implemented TaskViewModel methods.

--- ViewModels/TaskViewModel.cs
+++ ViewModels/TaskViewModel.cs
@@ -1,12 +1,52 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using LifeTasker.Models;
+using LifeTasker.Services;
 
 namespace LifeTasker.ViewModels
 {
     public class TaskViewModel
     {
+        public List<LifeTask> Tasks { get; private set; }
+
+        public event Action DataUpdated;
+
+        public TaskViewModel()
+        {
+            LoadData();
+        }
+
+        public void LoadData()
+        {
+            Tasks = Storage.LoadData();
+            PriorityManager.CheckDeadlines(Tasks);
+            Storage.SaveData(Tasks);
+        }
+
+        public void SaveData()
+        {
+            Storage.SaveData(Tasks);
+            DataUpdated?.Invoke();
+        }
+
+        public void AddTask(LifeTask task)
+        {
+            Tasks.Add(task);
+            SaveData();
+        }
+
+        public void UpdateTask(LifeTask existingTask, LifeTask newTask)
+        {
+            var index = Tasks.IndexOf(existingTask);
+            if (index >= 0)
+            {
+                Tasks[index] = newTask;
+                SaveData();
+            }
+        }
+
+        public void CheckDeadlines()
+        {
+            PriorityManager.CheckDeadlines(Tasks);
+            SaveData();
+        }
     }
 }
